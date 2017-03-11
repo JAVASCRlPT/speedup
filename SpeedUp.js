bd0e30ba8315ec776fe9749fde40094355ac66c9
JAVASCRlPT <sahrulsidik000@gmail.com>
2017-03-11 11:30:51 +0700

    Update SpeedUp.js

--- SpeedUp.js
+++ SpeedUp.js
@@ -1,6 +1,4 @@
-<script type='text/javascript'>
 //<![CDATA[
 $(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.sumber-akuka.blogspot.com/") {
         window.location.href = "http://www.sumber-akuka.blogspot.com"; }});
 //]]>
-</script>
